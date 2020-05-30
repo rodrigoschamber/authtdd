469e284e5b906cee91c88bb9896a718fa7367b14
Rodrigo Schamber <rodrigoschamber@gmail.com>
2020-05-30 11:53:23 -0300

    [ADD] There were created the first migration to the postgres database.

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host:'127.0.0.1',
+  username: 'postgres',
+  password: 'Postgres2018!',
+  database: 'postgres',
+  dialect: 'postgres',
+  //operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    undescoredAll: true
+  }
+};
